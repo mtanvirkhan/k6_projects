5d8a7f183c0e9d0235d3e243e6d85859e4a79fc2
M. Tanvir Khan <31180875+mtanvirkhan@users.noreply.github.com>
2025-08-02 16:37:28 +0600

    Add files via upload

---
+++ API Testing/API_device_list_BO.js
@@ -0,0 +1,65 @@
+import http from 'k6/http';
+import { group, check, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  vus: 1,
+  iterations: 1,
+  thresholds: {
+    vus: ['value==1'],
+    iteration_duration: ['avg<40000', 'min<40000', 'med<40000', 'max<40000', 'p(95)<40000', 'p(90)<40000']
+  }
+}
+
+export default function () {
+  group("Testing PT Console API", function () {
+
+    const params = {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0YW52aXIua2hhbkBiaml0Z3JvdXAuY29tIiwiZXhwIjoxNzI1MDg3NzczLCJpYXQiOjE3MjUwMDEzNzN9.TZcl1UihHe2a5YsUPASDRP884L61BTRGjAOmdVhzVGs'
+      },
+      timeout: '1000000ms'
+    };
+    //console.log(formData);
+
+    //sleep(30);
+
+    group("Import data checking", function () {
+      __ENV.imei = 543179977022555;
+      //__ENV.company = "US Shadman Corp";
+      //let res = http.get(`https://qa-global-consolebk.pocketalk.com/iotconsoleapi-global-bo/api/private/devicelist?searchText=${__ENV.imei}&currentPageNumber=0&rowLimitPerPage=100&colName=&sortDirection=&serverRegionIdList=`, params);
+      //let res1 = http.get(`https://qa-global-consolebk.pocketalk.com/iotconsoleapi-global-bo/api/private/devicelist?searchText=&currentPageNumber=0&rowLimitPerPage=100&colName=&sortDirection=&serverRegionIdList=`, params);
+      let res2 = http.get(`https://qa-global-consolebk.pocketalk.com/iotconsoleapi-global-bo/api/private/devicelist?searchText=warehouse11100000004&currentPageNumber=0&rowLimitPerPage=100&colName=&sortDirection=&serverRegionIdList=`, params);
+
+      console.log(`Response status: ${res2.status}`);
+      console.log(`Response body: ${res2.body}`);
+      //console.log(`Response body: ${res1.body}`);
+      console.log(`Total devices: ${JSON.parse(res2.body).total_devices}`);
+      console.log(`Total devices for US Shadman Corp: ${JSON.parse(res2.body).total_devices}`);
+      console.log(`Response time: ${res2.timings.waiting} ms`);
+      console.log(`Load time: ${res2.timings.duration} ms`);
+      
+      check(res2, {
+        'response status is 200': (r) => r.status === 200,
+        'response body contains success=true': (r) => JSON.parse(r.body).success === true,
+        'response body contains correct status': (r) => JSON.parse(r.body).status === 200,
+        'response body contains result_code OK': (r) => JSON.parse(r.body).result_code === 'OK',
+        'response body contains message OK': (r) => JSON.parse(r.body).message === 'OK',
+        'response body contains total_devices 1': (r) => JSON.parse(r.body).total_devices === 1,
+        'response body contains devices array': (r) => Array.isArray(JSON.parse(r.body).devices),
+        'response body contains correct server_region': (r) => JSON.parse(r.body).devices[0].server_region === 'US'
+      });
+    });
+  });
+}
+
+/*
+export function handleSummary(data) {
+  const currentTime = new Date().toISOString().replace(/[:.]/g, '-');
+  const outputDirectory = './HTML_device_founding_report';
+  return {
+      [`${outputDirectory}/SP16_JP_html_report_5K_devices_VUs_1_search${currentTime}.html`]: htmlReport(data),
+  };
+}
+  */
